6990fe803a97170e6fe4cf0a447ce1064826e95e
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-03 14:57:26 +0300

    In this commit z added cors module to our app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
@@ -11,6 +12,7 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
