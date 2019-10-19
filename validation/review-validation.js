4a90ea7760e383a9ba847d0fbb3d206eb176081f
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-19 22:47:17 +0300

    In this commit i added validation for range of rating

--- validation/review-validation.js
+++ validation/review-validation.js
@@ -5,8 +5,8 @@ module.exports = {
     check('comment', 'Comment cannot be empty or must be bigger then 3 letters')
       .isLength({ min: 3 })
       .not().isEmpty(),
-    check('rating', 'Please set rating to this place')
-      .not().isEmpty(),
+    check('rating', 'Please set rating to this place, range 0 - min, 5 - max')
+      .not().isEmpty().isInt({min: 0, max: 5}),
     check('placeId', 'Place id field is required')
       .not().isEmpty(),
   ],
