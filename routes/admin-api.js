760330478feeee2db7e16303d41c52ee43fc6278
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-25 18:31:30 +0300

    In this commit i create route which give me all moderators

--- routes/admin-api.js
+++ routes/admin-api.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
+
+const { getAllModerators } = require('../controllers/get-all-moderators');
+
+router.get('/moderators', getAllModerators);
 
 module.exports = router;
