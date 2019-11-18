fc8c02c5c768390501497c3f4700d18c9bbc2052
marianabob <marianabobeshko@gmail.com>
2019-11-18 23:25:49 +0200

    possibility to add new places for users added

--- models/places.model.js
+++ models/places.model.js
@@ -13,7 +13,7 @@ const placesSchema = new Schema({
   },
   ratingAvg: {
     type: Number,
-    required: true,
+    required: false,
   },
   photos: {
     type: Array,
@@ -21,31 +21,35 @@ const placesSchema = new Schema({
   },
   videos: {
     type: String,
-    required: true,
+    required: false,
   },
   rating: {
     type: Number,
-    required: true,
+    required: false,
   },
   reviews: {
     type: Array,
-    required: true,
+    required: false,
   },
   moderateBy: {
     type: String,
-    required: true,
+    required: false,
   },
+  isModerated: {
+    type: Boolean,
+    required:true,
+  },  
   createdBy: {
     type: String,
     required: true,
   },
   createdAt: {
-    type: String,
-    required: true,
+    type: Date,
+    default: Date.now
   },
   updateAt: {
-    type: String,
-    required: true,
+    type: Date,
+    default: Date.now
   },
   regionId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -60,10 +64,27 @@ const placesSchema = new Schema({
 const getSearchPlace = (search) => {
   return  places.find({name: new RegExp(search, 'i')}) 
 };
+
+const addNewPlaceToDb = (newPlace, token,files) =>{
+  const {isModerated, regionId, description, title} = newPlace;
+  let photoPathArr = [];
+  for(let photoPath of files){
+    photoPathArr.push(photoPath.path);
+  }
+  return  places.create({
+    isModerated,
+    regionId,
+    description,
+    photos:photoPathArr,
+    createdBy: token,
+    name: title
+  });
+}
 const places = mongoose.model('places', placesSchema);
 module.exports = {
   places,
-  getSearchPlace
+  getSearchPlace,
+  addNewPlaceToDb
 }
  
 
