6dc5063560c138e209ec19ac59f349fb7522eacd
Max Inno <maksym.hubach@gmail.com>
2019-11-20 00:52:13 +0200

    route and controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,12 +11,41 @@ exports.getAllUserData = async (req, res) => {
 
     const getUser = await userModel.findOne({ where: { id: userID } });
     if (!getUser) return res.status(500).send({ message: 'Can`t find user with this id' });
-    
+
     const userData = getUser.dataValues;
+    console.log(userData);
     delete userData.password;
 
     return res.status(200).send(userData);
   } catch (err) {
     return res.status(500).send({ message: 'Wrong user id or token' });
+  }
+};
+
+exports.editUserData = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const userID = await tokenService.verify(token);
+
+    const user = await userModel.findOne({ where: { id: userID } });
+    if (!user) return res.status(500).send({ message: 'Can`t find user with this id' });
+
+    const { name, surname, nickname, email } = req.body;
+    // for security reason -> don't load strange fields
+    const fromBody = { name, surname, nickname, email };
+
+    const toUpdate = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(fromBody)) {
+      if (value) {
+        toUpdate[key] = value;
+      }
+    }
+    user.update(toUpdate).then((result) => {
+      res.status(200).send(result.dataValues);
+    });
+  } catch (err) {
+    console.log(err)
+    res.status(500).send({ message: 'Illegal data passed' });
   }
 };
