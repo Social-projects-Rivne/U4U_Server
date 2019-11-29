b8077473296c8d6308f32e02e243e3c83ef5aede
Dmitry Chekurkov <chekurkov@gmail.com>
2019-11-29 16:11:28 +0200

    Fix

--- controllers/wish-list-controller.js
+++ controllers/wish-list-controller.js
@@ -6,78 +6,78 @@ const { getSearchPlace } = require('../models/places.model');
 //const { getToken } = require('../utils/token')
 
 exports.wishListGet = async (req, res) => {
+  const userJwt = req.header('authorization');
+  const tokenSplit = userJwt.split(" ");
+  const decodedJWT = await tokenService.verify(tokenSplit[1]);
+  // wishList.find({ userId: decodedJWT })
+  //     .then((lists) => {
+  //         res.status(200).send(lists);
+  //     })
+}
+exports.wishListPost = async (req, res) => {
+  try {
+    const { placeId, placeName, done, inProgress } = req.body;
     const userJwt = req.header('authorization');
     const tokenSplit = userJwt.split(" ");
     const decodedJWT = await tokenService.verify(tokenSplit[1]);
-    wishList.find({ userId: decodedJWT })
-        .then((lists) => {
-            res.status(200).send(lists);
-        })
-}
-exports.wishListPost = async (req, res) => {
-    try {
-        const { comment, done, inProgress } = req.body;
-        const userJwt = req.header('authorization');
-        const tokenSplit = userJwt.split(" ");
-        const decodedJWT = await tokenService.verify(tokenSplit[1]);
-        const mongoResponse = await wishList.create({
-            comment,
-            done,
-            inProgress,
-            userId: decodedJWT
-        });
-        await res.status(200).send({
-            message: 'Your wish is added',
-            id: mongoResponse.id
-        });
-    }
-    catch (e) {
-        res.status(500).send({ message: e });
-    }
+    const mongoResponse = await wishList.create({
+      placeId,
+      placeName,
+      done,
+      inProgress,
+      userId: decodedJWT
+    });
+    await res.status(200).send({
+      message: 'Your wish is added',
+      id: mongoResponse.id
+    });
+  }
+  catch (e) {
+    res.status(500).send({ message: e });
+  }
 };
 
 exports.wishListDelete = async (req, res) => {
 
-    try {
-        const { id } = req.params;
-        const userJwt = req.header('authorization');
-        const tokenSplit = userJwt.split(" ");
-        const decodedJWT = await tokenService.verify(tokenSplit[1]);
-        await wishList.findByIdAndDelete({
-            _id: id,
-            userId: decodedJWT
-        })
-        await res.status(200).send({ message: 'Your wish is deleted' });
-    }
-    catch (e) {
-        res.status(500).send({ message: e });
-    }
+  try {
+    const { id } = req.params;
+    const userJwt = req.header('authorization');
+    const tokenSplit = userJwt.split(" ");
+    const decodedJWT = await tokenService.verify(tokenSplit[1]);
+    await wishList.findByIdAndDelete({
+      _id: id,
+      userId: decodedJWT
+    })
+    await res.status(200).send({ message: 'Your wish is deleted' });
+  }
+  catch (e) {
+    res.status(500).send({ message: e });
+  }
 };
 
 exports.markItemAsDone = async (req, res) => {
-    try {
-        const { done, important, id } = req.body;
-        const userJwt = req.header('authorization');
-        const tokenSplit = userJwt.split(" ");
-        const decodedJWT = await tokenService.verify(tokenSplit[1]);
-        await wishList.findByIdAndUpdate(
-            { _id: id },
-            { done: done, inProgress: important },
-            { new: true });
-        await res.status(200).send({ message: 'Your wish is marked as done' });
-    }
-    catch (e) {
-        res.status(500).send({ message: e });
-    }
+  try {
+    const { done, important, id } = req.body;
+    const userJwt = req.header('authorization');
+    const tokenSplit = userJwt.split(" ");
+    const decodedJWT = await tokenService.verify(tokenSplit[1]);
+    await wishList.findByIdAndUpdate(
+      { _id: id },
+      { done: done, inProgress: important },
+      { new: true });
+    await res.status(200).send({ message: 'Your wish is marked as done' });
+  }
+  catch (e) {
+    res.status(500).send({ message: e });
+  }
 };
 
 exports.findPlaceByName = (req, res) => {
-    const { SearchInput } = req.params;
-    getSearchPlace(SearchInput).then((data) => {
-        res.status(200).send(data);
-    })
-        .catch(() => {
-            res.status(404).send({ message: 'Not Found' });
-        });
+  const { SearchInput } = req.params;
+  getSearchPlace(SearchInput).then((data) => {
+    res.status(200).send(data);
+  })
+    .catch(() => {
+      res.status(404).send({ message: 'Not Found' });
+    });
 }
-
