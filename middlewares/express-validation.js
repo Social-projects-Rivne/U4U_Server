2bf5ffc4224222a91d72d393a97672ae967773c8
Dima Basiuk <dimabasyk993@gmail.com>
2019-11-30 20:00:25 +0200

    clear uploads foder

--- middlewares/express-validation.js
+++ middlewares/express-validation.js
@@ -2,7 +2,6 @@ const { validationResult } = require('express-validator');
 
 module.exports = validate = (validations) => async (req, res, next) => {
   await Promise.all(validations.map((validation) => validation.run(req)));
-
   const errors = validationResult(req);
   if (errors.isEmpty()) {
     return next();
