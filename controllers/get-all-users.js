9dd456aede31659e1733525502cedd78f6c58dcf
marianabob <marianabobeshko@gmail.com>
2019-12-06 12:39:49 +0200

    get all unblocked users method changed

--- controllers/get-all-users.js
+++ controllers/get-all-users.js
@@ -1,22 +1,12 @@
-const user = require('../models/user.model');
+const sequelize = require('../config/postgre');
 
 exports.getAllUsers = (req, res) => {
-  user.findAll({
-    order: [
-      ["id", "ASC"]
-    ]
+sequelize.query('SELECT users. * FROM users LEFT JOIN bans ON users.id = bans.user_id WHERE bans.user_id IS NULL')
+  .then((data) => {
+    res.status(200).send(data[0]);
   })
-    .then((data) => {
-      res.status(200).send(data);
-    })
-    .catch(() => {
-      res.status(404).send({ message: 'Not Found' });
-    });
+  .catch(() => {
+    res.status(404).send({ message: 'Not Found' });
+  });
 };
-// sequelize.query('SELECT * FROM users LEFT JOIN bans ON users.id = bans.user_id WHERE user_id IS NULL')
-//   .then((data) => {
-//     res.status(200).send(data[0]);
-//   })
-//   .catch(() => {
-//     res.status(404).send({ message: 'Not Found' });
-//   });
+
